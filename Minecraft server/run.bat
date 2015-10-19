027885bead4abe1f17281d0a3130ce48157c3379
Arthas <Beatsbysd@gmail.com>
2015-10-19 03:19:13 -0400

    This is our base server right now
    Will start building and coding tommorow I'm Sleepy

---
+++ Minecraft server/run.bat
@@ -0,0 +1,11 @@
+@echo off
+title Minecraft Server Console [Bukkit 1.8.8] @xSplayd
+color 0e
+cls
+java -Xmx1024M -jar craftbukkit.jar -o true
+echo.
+echo ------------------------------------------------------------------
+echo Press any key to end console // Subscribe: http://bit.ly/subsplayd
+echo ------------------------------------------------------------------
+PAUSE>nul
+exit()
